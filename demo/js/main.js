a7300845e78137e9f774714ce24cdc45b3c4b471
Jonathan Kim <jonathan.kim@fusepump.com>
2013-05-01 18:38:56 +0100

    Initial commit of presentation and demo

---
+++ demo/js/main.js
@@ -0,0 +1,22 @@
+$(function() {
+    // Welcome to the console
+    console.log('-------------------------------------------------------------');
+    console.log('Welcome to the JavaScript demo!');
+    console.log('-------------------------------------------------------------');
+
+    $('.code .content').each(function() {
+        CodeMirror.fromTextArea(this, {
+            mode: "javascript"
+        });
+    });
+
+    // Run arbitrary bits of code in the console
+    $('.run').each(function() {
+        $(this).click(function() {
+            var editor = $(this).parents('.code').find('.CodeMirror').get(0).CodeMirror;
+            var code = editor.getValue();
+            eval(code);
+        });
+    });
+
+});
---
+++ presentation.md
@@ -0,0 +1,22 @@
+# JavaScript 101
+
+A short introduction to JavaScript
+
+# History
+
+# Syntax
+
+# Functions
+
+# Pitfalls
+## Types
+
+# "this"
+
+# Scope
+
+# Closures
+
+# JsHint
+
+# Read More
